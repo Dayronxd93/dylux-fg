edd98bbebb3a6b971c02e1af80f0039479899f93
Dayronxd93 <106130040+Dayronxd93@users.noreply.github.com>
2022-05-24 09:52:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true], 
+  ['13373750352', 'Alex', true], 
   ['5492622689586'], 
   ['5493885839638']
 ] //Numeros de owner 
